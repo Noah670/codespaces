1c085554b8f6ba92ead72b56a68bbe0b79e8d897
Noah <noahrules96950@gmail.com>
2022-07-25 15:54:08 -0700

    Testing codespaces

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
+let jackwario = 'snowing on Mount Fuji'
 const haikus = require('./haikus.json');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
+
+// adding a sample test case
+
 
 app.get('/', (req, res) => {
   res.render('index', {haikus: haikus});
